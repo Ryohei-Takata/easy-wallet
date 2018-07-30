9e166394eae0cca231b5ff7ee0f018d591c29794
SC-HOLDINGS\ryo-takata <ryo-takata@sc-ko.jp>
2018-07-30 20:16:06 +0900

    GitHubPages設定
    GitHubPages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eabb2f2ec95424de4448ca5333d8e338.js"
+  "./precache-manifest.b5631b42491cbc749ac4c033b51e5550.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -27,6 +27,19 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "f1c7a1ff780e8ddc62898c8c4710d47c"
+  },
+  {
+    "url": "precache-manifest.eabb2f2ec95424de4448ca5333d8e338.js",
+    "revision": "eabb2f2ec95424de4448ca5333d8e338"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "2c56096fd72c4a7d8411608f8cda9272"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
